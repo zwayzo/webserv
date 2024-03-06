7bed972c7dd744267425ce57237478865fc14370
Mohammed Azzeddine <moazzedd@c4r5p2.1337.ma>
2024-03-06 17:05:43 +0100

    finish doc

--- multuplixing.cpp
+++ multuplixing.cpp
@@ -26,10 +26,11 @@ void multuplixing(conf* conf)
     socklen_t addrlen;
     // socklen_t addr_size;
     struct sockaddr_storage remoteaddr; // client address
-    struct timeval tv;
+    // struct timeval tv;
     fd_set master;    // master file descriptor list
     fd_set read_fds;  // temp file descriptor list for select()
-
+    int nbytes;
+    char buf[256];    // buffer for client data
     for (int i = 0; i < conf->serversNumber; i++)
     {
         memset(&hints[i], 0, sizeof(struct addrinfo));
@@ -61,7 +62,6 @@ void multuplixing(conf* conf)
         else 
             std::cout << "binding...\n";
         freeaddrinfo(result[i]);
-        // connect(serverSocket[i], result[i]->ai_addr, result[i]->ai_addrlen);
         if ((listen_fd[i] = listen(serverSocket[i], 2)) == -1)
             throw ("listen");
         else
@@ -76,16 +76,50 @@ void multuplixing(conf* conf)
             read_fds = master;
             if (select(maxfd, &read_fds, NULL, NULL, NULL) == -1)
                 throw ("ERROR IN SELECT...!\n");
+            // std::cout 
             for (int j = 0;j < maxfd; j++){
                 if (FD_ISSET(j, &read_fds)) // if the fd is in the set
                 {
-                    if (j == serverSocket[i]) //the fd is in the set
+                    std::cout << "her\n";
+                    if (j == serverSocket[i]) //the fd is in the set handel new connection
                     {// handel new connections
                         addrlen = sizeof(remoteaddr);
-                        newFd =accept(serverSocket[i], (struct sockaddr *)&remoteaddr, &addrlen);\
-                        
-                }
+                        newFd = accept(serverSocket[i], (struct sockaddr *)&remoteaddr, &addrlen);
+                        if (newFd == -1)
+                            throw ("ERROR IN ACCEPT FUNCTION...\n");
+                        FD_SET(newFd, &master); //now we have fd that have the request and fd who still waiting
+                        if (newFd > maxfd)
+                            maxfd = newFd;
+                    }
+                    else{
+                        std::cout << "her1\n";
+                        if ((nbytes = recv(j, buf, sizeof(buf), 0)) <= 0)
+                        {
+                            // close (j);
+                            FD_CLR(j, &master);
+                            if (nbytes == 0)
+                                throw ("SOCKT IS HANGING...(RECV)\n");
+                            else
+                                throw ("ERROR IN (RECV)\n");
+                        }
+                        else{
+                            for (int x = 0; x < maxfd; x++){
+                                if (FD_ISSET(x, &master)){
+                                    if (x != serverSocket[i] && x != j){
+                                        if (send(x, buf, nbytes, 0) == -1)
+                                            throw ("ERROR IN SENDING...\n");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }//handel data from client
+            }//got new upcoming connections
+        }//looping through fd's
+    }//while 1
+}
 
+                        
         // if (FD_ISSET(serverSocket[i], &read_fds))
         //     printf("A key was pressed!\n");
         // else
@@ -99,7 +133,6 @@ void multuplixing(conf* conf)
         // while (1);
 
         // std::cout << serverSocket[i] << " addr:" << conf->ser[i].socketAddr << '\n';
-    }
     // for (int i = 0;i < conf->serversNumber; i++)
         // freeaddrinfo(result[i]);
 
@@ -107,6 +140,6 @@ void multuplixing(conf* conf)
 //     memset(&hints, 0, sizeof(struct addrinfo));
 //     hints.ai_family = AF_UNSPEC;     // Allow IPv4 or IPv6
 //     hints.ai_socktype = SOCK_STREAM; // Stream socket (TCP)
-}
+
 
 
